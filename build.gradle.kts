4932f01b4799b53cbbd94be9d3a2c525fdfd4e96
kkreoffice-droid <kkreoffice@gmail.com>
2025-11-15 01:37:21 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    kotlin("android") version "1.9.22" apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "GPSNotes"
+include(":app")
